aae138156efec4e0b7e2966068e9cb08e86c606d
kpoxo6op <kpoxo6op@gmail.com>
2019-11-16 22:16:21 +1300

    frontpage refactor

--- site/web/app/themes/welcomedog-theme/app/Controllers/FrontPage.php
+++ site/web/app/themes/welcomedog-theme/app/Controllers/FrontPage.php
@@ -5,20 +5,24 @@ namespace App\Controllers;
 use Sober\Controller\Controller;
 class FrontPage extends Controller {
     public function latest_dogplaces() {
-        $args = array(
+        $args = get_posts([
             'post_type' => 'dogplace',
             'orderby'   => 'date',
             'order'     => 'desc',
             'posts_per_page' => 3,
-        );
-        $the_query = new \WP_Query($args);
-        return $the_query;
+        ]);
 
-    /* not sure how to implement this
-    public static function cpt_terms() {
-        $dogplace_terms_array = "CPT dogplace category and tags array";
-        return $dogplace_terms_array;
-    }
-    */
+        return array_map(function ($post) {
+            return [
+                'content'   => apply_filters('the_content', $post->post_content),
+                'excerpt'   => apply_filters('the_excerpt', $post->post_content),
+                'thumbnail' => get_the_post_thumbnail($post->ID, 'large'),
+                'thumb_url' => get_the_post_thumbnail_url($post->ID, 'large'),
+                'alt'       => get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true),
+                'title'     => apply_filters('the_title', $post->post_title),
+                'permalink' => apply_filters('permalink', get_permalink($post)),
+                'type'      => get_the_terms($post->ID, 'dogplace-type'),
+            ];
+        }, $args);
     }
 }
--- site/web/app/themes/welcomedog-theme/resources/views/partials/recently-added.blade.php
+++ site/web/app/themes/welcomedog-theme/resources/views/partials/recently-added.blade.php
@@ -2,26 +2,26 @@
   <h2>Recently Added</h2>
 </div>  
 <div class="block md:flex justify-between md:-mx-2">
-    @while($latest_dogplaces->have_posts()) @php($latest_dogplaces->the_post())
+    @foreach($latest_dogplaces as $dp)
     <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
         <div class="bg-white rounded-lg overflow-hidden shadow relative">
-            <a href="{{ get_permalink() }}">{!! the_post_thumbnail(null, ['class' => 'h-56 w-full object-cover object-center']) !!}</a>
+            <a href="{!! $dp['permalink'] !!}">
+                <img src="{!! $dp['thumb_url'] !!}" class="h-56 w-full object-cover object-center wp-post-image" alt="{!! $dp['alt'] !!}">
+            </a>
             <div class="p-4 h-auto md:h-40 lg:h-48">
-                <a href="{{ get_permalink() }}" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">{!! get_the_title() !!}</a>
+                <a href="{!! $dp['permalink'] !!}" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">{!! $dp['title'] !!}</a>
                 <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
-                    @foreach (get_the_terms(get_the_ID(), 'dogplace-type') as $category)
-                        <a class="" href="#">{!! $category->name !!}</a>
+                    @foreach ($dp['type'] as $type)
+                    <a class="" href="#">{!! $type->name !!}</a>
                     @endforeach
-                    {!! the_excerpt() !!}
+                    <p>{!! $dp['excerpt'] !!}</p>
                 </div>
                 <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block">
-                    @foreach (get_the_terms(get_the_ID(), 'tag') as $tag)
-                        <!-- <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#{!! $tag->name !!}</a> -->
-                    @endforeach
+                    <!---<a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#hashtag</a>--> 
                 </div>
-            </div> 
+            </div>
         </div>
     </div>
-    @endwhile
+    @endforeach
     @php(wp_reset_postdata())
-</div> 
+</div>
